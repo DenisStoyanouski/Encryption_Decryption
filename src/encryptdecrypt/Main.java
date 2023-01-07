1953483544ec50463e07484187b2b2ae54acf3ce
Denis Stoyanouski <vandenst@tut.by>
2023-01-07 22:33:34 +0300

    added method decrypt

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -11,29 +11,43 @@ public class Main {
     private static String text;
 
     public static void main(String[] args) {
+        String mode = input();
         text = input();
         key = Integer.parseInt(input());
-        System.out.println(encrypt());
 
-    }
-
-    private static String encrypt() {
-        StringBuilder decrypt = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) >= 'a' && text.charAt(i) <= 'z') {
-                char letter = (char) (text.charAt(i) + key);
-                if (letter > 122) {
-                    letter -= 26;
-                }
-                decrypt.append(letter);
-            } else {
-                decrypt.append(text.charAt(i));
-            }
+        switch(mode) {
+            case "enc" :
+                System.out.println(encrypt());
+            break;
+            case "dec" :
+                System.out.println(decrypt());
+            break;
+            default:
+                System.out.println("Unknown mode");
+                break;
         }
-        return decrypt.toString();
+
     }
 
     private static String input() {
         return scanner.nextLine();
+    }
+
+    private static String encrypt() {
+        StringBuilder encrypt = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+                char letter = (char) (text.charAt(i) + key);
+                encrypt.append(letter);
+        }
+        return encrypt.toString();
+    }
+
+    private static String decrypt() {
+        StringBuilder decrypt = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char letter = (char) (text.charAt(i) - key);
+            decrypt.append(letter);
+        }
+        return decrypt.toString();
     }
 }
